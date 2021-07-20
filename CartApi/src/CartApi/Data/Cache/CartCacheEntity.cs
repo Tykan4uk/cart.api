aa6bacf250c866746812b0c8ee97a62020143289
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-20 22:02:26 +0300

    CartCacheEntity: changing id type =>string, change hashset of id on List of CartProductModel

--- CartApi/src/CartApi/Data/Cache/CartCacheEntity.cs
+++ CartApi/src/CartApi/Data/Cache/CartCacheEntity.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
+using CartApi.Models;
 
 namespace CartApi.Data.Cache
 {
     public class CartCacheEntity : ICacheEntity
     {
-        public int UserId { get; set; }
-        public HashSet<int> GameIdList { get; set; } = null!;
+        public string UserId { get; set; }
+        public List<CartProductModel> CartProducts { get; set; } = null!;
     }
 }
