b39269dda9d530d1aa51ae0fe0512e9524a213d9
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-20 21:21:54 +0300

    Adding class ProductModel

---
+++ CartApi/src/CartApi/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using CartApi.Common.Enums;
+
+namespace CartApi.Models
+{
+    public class ProductModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ProductTypeEnum Type { get; set; }
+    }
+}
