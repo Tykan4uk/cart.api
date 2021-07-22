5f2159f8ab3c86a22676869df1acb9eee5517644
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-22 23:33:53 +0300

    Startup: adding IS4 settings

--- CartApi/src/CartApi/Startup.cs
+++ CartApi/src/CartApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace CartApi
@@ -26,6 +27,32 @@ namespace CartApi
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // accepts any access token issued by identity server
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+
+            // adds an authorization policy to make sure the token is for scope 'api1'
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "cartapi.cartapi");
+                });
+                options.AddPolicy("ApiScopeBff", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "cartapi.cartapibff");
+                });
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -51,6 +78,7 @@ namespace CartApi
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
